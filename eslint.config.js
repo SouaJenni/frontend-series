fcd2f5ba0fa335c3c0a8a48022c9d070b9d2386d
SouaJenni <jennifervialli1@gmail.com>
2025-07-12 21:19:08 -0300

    Adicionando regras para não permitir importes e variáveis não utilizadas no ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
+import pluginUnusedImports from 'eslint-plugin-unused-imports';
 import { defineConfig } from 'eslint/config';
-
 
 export default defineConfig([
     js.configs.recommended,
@@ -10,24 +10,43 @@ export default defineConfig([
     {
         files: ['**/*.{js,mjs,cjs,jsx}'],
         languageOptions: {
-            ecmaVersion: 12,
+            ecmaVersion: 2021,
             sourceType: 'module',
-            globals: globals.browser
+            globals: globals.browser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
         },
         settings: {
             react: {
-                version: '19.1'
-            }
+                version: '19.1',
+            },
         },
         plugins: {
             js,
-            react: pluginReact
+            react: pluginReact,
+            'unused-imports': pluginUnusedImports,
         },
         rules: {
             indent: ['error', 4],
             semi: ['error', 'always'],
             quotes: ['error', 'single'],
-            'react/jsx-key': 'error'
-        }
-    }
+
+            'react/jsx-key': 'error',
+
+            'no-unused-vars': 'off',
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
 ]);
